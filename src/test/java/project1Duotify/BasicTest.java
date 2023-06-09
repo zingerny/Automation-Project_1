d5c784cd3d6b1619ec37847cd48b7226d3f41798
zinam <zingerny@gmail.com>
2023-06-08 22:23:17 -0400

    "regroup"

--- src/test/java/project1Duotify/BasicTest.java
+++ src/test/java/project1Duotify/BasicTest.java
@@ -1,10 +1,10 @@
+package project1Duotify;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-
-import java.sql.SQLOutput;
 
 public class BasicTest {
     public static void main(String[] args) throws InterruptedException {
--- src/test/java/project1Duotify/RandomPassword.java
+++ src/test/java/project1Duotify/RandomPassword.java
@@ -1,3 +1,5 @@
+package project1Duotify;
+
 public class RandomPassword {
 
     static String randomPassword = "";
